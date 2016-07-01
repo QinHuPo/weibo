99371c0f7df45fa0d5546f3935069957b8a83636
“qhp git config  --global user.email “qhp_work@163.com <756918492@qq.com>
2016-07-01 14:12:58 +0800

    创建了QHPModel类

---
+++ weibo/weibo/QHPModel.h
@@ -0,0 +1,13 @@
+//
+//  QHPModel.h
+//  weibo
+//
+//  Created by 秦虎坡 on 16/7/1.
+//  Copyright © 2016年 秦虎坡. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QHPModel : NSObject
+
+@end
---
+++ weibo/weibo/QHPModel.m
@@ -0,0 +1,13 @@
+//
+//  QHPModel.m
+//  weibo
+//
+//  Created by 秦虎坡 on 16/7/1.
+//  Copyright © 2016年 秦虎坡. All rights reserved.
+//
+
+#import "QHPModel.h"
+
+@implementation QHPModel
+
+@end
