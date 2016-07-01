aa8e857b26d6c58f297da56843f997bce781e08a
“qhp git config  --global user.email “qhp_work@163.com <756918492@qq.com>
2016-07-01 13:06:55 +0800

    1111

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by 秦虎坡 on 16/7/1.
+//  Copyright © 2016年 秦虎坡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by 秦虎坡 on 16/7/1.
+//  Copyright © 2016年 秦虎坡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
